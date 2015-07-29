0c94c1bfe1262a125fbf0cace1db33b954d3240d
Albert Veli <albert@iMac.local>
2015-07-29 15:43:05 +0200

    Initial commit

---
+++ cursesbreaker.py
@@ -0,0 +1,434 @@
+#!/usr/bin/env python
+#
+# Simple code-breaking python curses game inspired
+# by the mobile phone game NSA CryptoChallenge.
+#
+# Albert Veli
+# Setting Orange, the 64th day of Confusion in the YOLD 3181
+
+import curses
+import time
+import random
+
+# Add current directory to pythonpath
+import os.path
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# and import quotes from quotes.py
+from quotes import quotes
+
+# --- Globals ---
+scr = None
+fp = None
+category = None
+quote = None
+qu = None
+numquotes = None
+spacing = 1
+startpos = [2, 4]
+pos = None
+rchar = None
+perm = None
+start_time = None
+guessd = None
+missing = 0
+errors = None
+timestr = None
+final_time = None
+label = None
+state = 'Running'
+
+# --- Gui helpers ---
+
+def init_gui():
+    global scr
+    global fp
+    scr = curses.initscr()
+    scr.keypad(True)
+    # Timeout getkey() every second
+    # so time string is updated every second.
+    scr.timeout(1000)
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(False)
+    # Comment the row below to skip debug.log.
+    fp = open('debug.log', 'w')
+
+def cleanup_gui():
+    global scr
+    curses.nocbreak()
+    scr.keypad(False)
+    scr.clear()
+    curses.echo()
+    curses.endwin()
+
+# --- Helpers ---
+
+def get_numquotes():
+    """ Return number of quotes in quotes """
+    global numquotes
+
+    # This will only be True the first time.
+    # On following calls numquotes will be
+    # returned immediately.
+    if numquotes == None:
+        numquotes = 0
+        for line in quotes.split('\n'):
+            line.strip()
+            if (not line.startswith('#')) and (':' in line):
+                numquotes += 1
+
+    return numquotes
+
+def get_quote():
+    """ Choose quote number n at random """
+    n = random.randrange(get_numquotes())
+    i = 0
+    for line in quotes.split('\n'):
+        line.strip()
+        if (not line.startswith('#')) and (':' in line):
+            if i == n:
+                break
+            else:
+                i += 1
+    # Now line is the quote we want
+    # Split it into a category and a quote part
+    category, quote = line.split(':')
+    category = category.strip()
+    quote = quote.strip()
+    return (category, quote)
+
+def get_randperm():
+    """
+    Return a dictionary with mapping of A-Z
+    to A-Z in random order (permutation).
+    In other words, the keys of the dictionary
+    are cleartext letters and the values are
+    ciphertext letters.
+    """
+    vals = list()
+    while True:
+        n = random.randrange(26)
+        if not n in vals:
+            vals.append(n)
+            if len(vals) == 26:
+                break
+    # Convert 0-25 to A-Z
+    for i in range(len(vals)):
+        vals[i] = chr(vals[i] + ord('A'))
+
+    # Create dictionary A-Z -> vals
+    keys = list()
+    for i in range(len(vals)):
+        keys.append(chr(i + ord('A')))
+    # keys and vals must be of equal size
+    # for this to work. And they are.
+    return dict(zip(keys, vals))
+
+def advance_cursor(x, nl = False):
+    """
+    Advance cursor x steps forward.
+    If nl is True then go to beginning
+    of next line.
+    """
+    global pos
+    if nl:
+        pos[0] = startpos[0]
+        pos[1] += 1
+    else:
+        pos[0] += x
+        if pos[0] >= maxx:
+            pos[0] -= maxx
+            pos[1] += 1
+
+def out_msg(msg, attr = False, nl = False):
+    """
+    Print msg at current pos. attr can be one of:
+    A_BLINK     - Blinking (not annoying at all)
+    A_BOLD      - Bold
+    A_DIM       - Half bright
+    A_REVERSE   - Reversed
+    A_STANDOUT  - Highlighted
+    A_UNDERLINE - Underlined
+    """
+    global pos
+    if attr:
+        scr.addstr(pos[1], pos[0], msg, attr)
+    else:
+        scr.addstr(pos[1], pos[0], msg)
+    advance_cursor(len(msg), nl = nl)
+
+def print_clear(s):
+    """
+    Print string s in cleartext. For each char
+    If char is in guessd, print it. If guess is
+    wrong print in reverse. Print space if char
+    is not in guessd. If char is not in A-Z, just
+    print it out (numbers, special characters).
+    """
+    global missing
+    global errors
+    for c in s:
+        attr = False
+        if c in perm:
+            p = perm[c]
+            if p in guessd:
+                oc = c
+                c = guessd[p]
+                if oc != c:
+                    attr = curses.A_REVERSE
+                    # Count errors at same time
+                    errors.add(c)
+            else:
+                c = ' '
+                # Count missing at same time
+                missing += 1
+        out_msg(c, attr)
+        advance_cursor(1)
+    out_msg('', nl = True)
+
+def print_perm(s):
+    """
+    Print string s permuted by dict p.
+    If a char is not in p, print it as is.
+    """
+    for c in s:
+        if c in perm and perm[c] == rchar:
+            attr = curses.A_REVERSE
+        else:
+            attr = False
+        if c in perm:
+            out_msg(perm[c], attr)
+        else:
+            out_msg(c, attr)
+        advance_cursor(1)
+    out_msg('', nl = True)
+
+def get_time(ts, penalty):
+    """ Add penalty to ts and return new timestring. """
+    # ts in format 'mm:ss'
+    m, s = ts.split(':')
+    m = int(m)
+    s = int(s)
+    s += penalty
+    while s > 59:
+        s -= 60
+        m += 1
+    ts = "%02d:%02d" % (m, s)
+    return ts
+
+def print_interleaved(s, width):
+    """
+    Print cleartext (s) and ciphertext
+    interleaved. Break lines at first
+    space before width characters.
+    """
+    global missing
+    global errors
+    global frame
+    global final_time
+    global pos
+    global label
+    global state
+
+    i = 0
+    missing = 0
+    errors = set()
+    while i < len(s):
+        # Trim leading spaces
+        while (s[i] == ' '):
+            i += 1
+        end = i + width
+        # Last line
+        if end > len(s):
+            end = len(s)
+
+        # Don't cut words in half
+        if (end < len(s)) and (s[end] != ' '):
+            # Move end back to last space
+            while s[end] != ' ':
+                end -= 1
+        out_msg('', nl = True)
+
+        # Clear text
+        print_clear(s[i:end])
+
+        # Cipher text
+        print_perm(s[i:end])
+
+        i += end - i + 1
+
+    # Check if we're done yet
+    if missing == 0:
+        final_time = get_time(timestr, len(errors) * 10)
+        label = 'Well done! Errors = ' + str(len(errors)) + '. Time = ' + final_time + '. 1 = new game. 4 = quit'
+        state = 'Finished'
+
+def dbg(s):
+    global fp
+    if fp:
+        fp.write(s + '\n')
+        fp.flush()
+
+def main_game_handler():
+    global pos
+    global timestr
+
+    scr.clear()
+    scr.border(0)
+
+    # Draw header
+    pos = [maxx // 2 - 11, 1]
+    out_msg('PYTHON CRYPTO CHALLENGE', curses.A_REVERSE)
+    # Category
+    pos = [startpos[0], 2]
+    out_msg(category)
+    # Time
+    pos = [maxx - 7, 2]
+    elapsed = int(time.time() - start_time)
+    timestr = "%02d:%02d" % (elapsed // 60, elapsed % 60)
+    out_msg(timestr)
+    # Line separator
+    pos = [startpos[0], 3]
+    for i in range(maxx - 4):
+        out_msg('_')
+
+    # Draw crypto
+    pos[0] = startpos[0]
+    pos[1] = startpos[1]
+    print_interleaved(qu, maxx // 2 - 5)
+
+    # Status bar
+    pos = [startpos[0], maxy - 2]
+    if label:
+        out_msg(label, curses.A_REVERSE)
+    scr.refresh()
+
+def new_game():
+    """ Run one game round """
+    global category
+    global quote
+    global qu
+    global perm
+    global start_time
+    global guessd
+    global state
+
+    # Init globals
+    category, quote = get_quote()
+    # If you really want to, you can cheat
+    # by uncommenting the print lines below
+    dbg(category)
+    dbg(quote)
+    qu = quote.upper()
+    perm = get_randperm()
+    guessd = dict()
+    clear_handler()
+    # Start with one hint
+    hint_handler(False)
+    start_time = time.time()
+    state = 'Running'
+
+def key_handler(key):
+    global label
+    global rchar
+    global guessd
+    key = key.upper()
+    # Delete/space/backspace resets replace state
+    if (key == 'KEY_DC') or (key == ' ') or (ord(key) == 8) or (ord(key) == 127):
+        clear_handler()
+    elif not rchar:
+        if (key >= 'A') and (key <= 'Z'):
+            rchar = key
+            label = 'Replace ' + rchar + ' with'
+    else:
+        if (key >= 'A') and (key <= 'Z'):
+            label = 'Replace ' + rchar + ' with ' + key
+            guessd[rchar] = key
+            #dbg(str(guessd))
+            rchar = None
+            clear_handler()
+
+def hint_handler(penalty = True):
+    global start_time
+    global guessd
+    if penalty:
+        start_time -= 15
+    while True:
+        c = chr(random.randrange(26) + ord('A'))
+        p = perm[c]
+        if (c in qu) and (not p in guessd):
+            guessd[p] = c
+            return None
+
+def clear_handler():
+    """
+    Remove mapping for rchar.
+    """
+    global guessd
+    global rchar
+    global label
+    if rchar and (rchar in guessd):
+        guessd.pop(rchar)
+    rchar = None
+    label = 'Type letter to replace'
+
+def print_help():
+    print """
+Python Code Breaker
+
+Keys:
+ 1   - New Game.
+ 2   - Hint (+15s).
+ 4   - Quit.
+ Del - Clear currently highlighted letter.
+       Space/Backspace also clears.
+ A-Z - Letter to replace.
+       Case doesn't matter.
+
+Tips:
+* Look for single-letter words, double letters
+  within words and apostrophes.
+* Look for common words like the, be, to, of, and,
+  a, in, that, have, I, it, for, not etc.
+  http://www.oxforddictionaries.com/words/the-oec-facts-about-the-language
+* The more you play the more trends will become apparent.
+
+Press Enter to start game
+"""
+    raw_input()
+
+# --- Main ---
+
+print_help()
+random.seed()
+init_gui()
+
+key = '0'
+
+new_game()
+
+# Game loop. Fake event-driven program.
+while key != '4':
+    maxy, maxx = scr.getmaxyx()
+    if state == 'Running':
+        # This is where everything happens
+        main_game_handler()
+
+    # try/except needed to catch resize of terminal
+    # window while in scr.getkey()
+    try:
+        key = scr.getkey()
+    except:
+        key = '0'
+        maxy, maxx = scr.getmaxyx()
+        #dbg('getkey exception, timeout/resize')
+    if key == '1':
+        new_game()
+    elif key == '2' and state == 'Running':
+        hint_handler()
+    elif key != '0' and key != '4' and state == 'Running':
+        #dbg('You entered ' + key)
+        key_handler(key)
+
+cleanup_gui()
---
+++ quotes.py
@@ -0,0 +1,133 @@
+# One quote per line. Begin with category.
+# Separate category and quote with a colon character.
+# Quotes may not contain the colon character or the
+# universe might explode. At least the quote parser.
+# Further it may not contain tabs and only some special
+# characters, like comma, quote, semicolon and period.
+# Feel free to add more quotes, but remember that some
+# quotes are harder to solve than other.
+# Too short ones almost impossible.
+quotes = """
+Abraham Lincoln: In the end, it's not the years in your life that count. It's the life in your years.
+
+Abraham Lincoln: Character is like a tree and reputation like a shadow. The shadow is what we think of it; the tree is the real thing.
+
+Abraham Lincoln: If this is coffee, please bring me some tea; but if this is tea, please bring me some coffee.
+
+Abraham Lincoln: Better to remain silent and be thought a fool than to speak out and remove all doubt.
+
+Aristotle: It is the mark of an educated mind to be able to entertain a thought without accepting it.
+
+Aristotle: The worst form of inequality is to try to make unequal things equal.
+
+Aristotle: In a democracy the poor will have more power than the rich, because there are more of them, and the will of the majority is supreme.
+
+Asimov: The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.
+
+Asimov: Part of the inhumanity of the computer is that, once it is competently programmed and working smoothly, it is completely honest.
+
+Asimov: No sensible decision can be made any longer without taking into account not only the world as it is, but the world as it will be.
+
+Asimov: When I read about the way in which library funds are being cut and cut, I can only think that American society has found one more way to destroy itself.
+
+Asimov: I don't believe in personal immortality; the only way I expect to have some version of such a thing is through my books.
+
+Buddha: Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.
+
+Buddha: There are only two mistakes one can make along the road to truth; not going all the way, and not starting.
+
+Carl Jung: Even a happy life cannot be without a measure of darkness, and the word happy would lose its meaning if it were not balanced by sadness.
+
+Carl Jung: The meeting of two personalities is like the contact of two chemical substances; if there is any reaction, both are transformed.
+
+Carl Sagan: We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology.
+
+Carl Sagan: If you wish to make an apple pie from scratch, you must first invent the Universe.
+
+Confucius: Our greatest glory is not in never falling, but in rising every time we fall.
+
+Confucius: To know what you know and what you do not know, that is true knowledge.
+
+Confucius: If you think in terms of a year, plant a seed; if in terms of ten years, plant trees; if in terms of 100 years, teach the people.
+
+Cybersecurity: A password is like a toothbrush. Choose a good one. Don't share it with anyone. Change it occasionally.
+
+Dr. Seuss: Today you are you! That is truer than true! There is no one alive who is you-er than you!
+
+Dr. Seuss: From there to here, and here to there, funny things are everywhere.
+
+Einstein: You have to learn the rules of the game. And then you have to play better than anyone else.
+
+Einstein: Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.
+
+Einstein: The only reason for the existence of time is so that everything doesn't happen at once.
+
+Gandhi: Happiness is when what you think, what you say, and what you do are in harmony.
+
+Gandhi: There is a higher court than courts of justice and that is the court of conscience. It supercedes all other courts.
+
+Funny Quotes: I believe that if life gives you lemons, you should make lemonade... And try to find somebody whose life has given them vodka, and have a party.
+
+Funny Quotes: The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.
+
+Funny Quotes: Roses are red, violets are blue, I'm schizophrenic, and so am I.
+
+Funny Quotes: If women ran the world we wouldn't have wars, just intense negotiations every 28 days.
+
+Funny Quotes: People are software running on hardware, both built for lives we no longer live.
+
+Funny Quotes: That intelligent creatures exist in outer space is proven by the fact that they have'nt contacted us.
+
+Martin Luther King: Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.
+
+Martin Luther King: The ultimate measure of a man is not where he stands in moments of comfort and convenience, but where he stands at times of challenge and controversy.
+
+Martin Luther King: I have a dream that my children will one day live in a nation where they will not be judged by the color of their skin, but by the content of their character.
+
+Martin Luther King: Law and order exist for the purpose of establishing justice and when they fail in this purpose they become the dangerously structured dams that block the flow of social progress.
+
+Martin Luther King: Nonviolence means avoiding not only external physical violence but also internal violence of spirit. You not only refuse to shoot a man, but you refuse to hate him.
+
+Monty Python: We are no longer the knights who say ni! We are now the knights who say ekki-ekki-ekki-pitang-zoom-boing!
+
+Monty Python: Yes. Shrubberies are my trade. I am a shrubber. My name is Rogerthe Shrubber. I arrange, design, and sell shrubberies.
+
+Monty Python: If she weighs the same as a duck... she's made of wood... (and therefore) a witch!
+
+Monty Python: I cut down trees, I skip and jump, I like to press wild flowers. I put on women's clothing and hang around in bars.
+
+Science & Technology: Different names for the number 0 include zero, nought, naught, nil, zilch and zip.
+
+Science & Technology: The number 5 is pronounced as 'Ha' in Thai language. 555 is also used by some as slang for 'HaHaHa'.
+
+Science & Technology: Googol is the term used for a number 1 followed by 100 zeros and it was used by a nine-year old, Milton Sirotta, in 1940.
+
+Science & Technology: The name 'zero' derives from the Arabic word sifr which also gave us the English word cipher meaning a secret way of writing.
+
+Science & Technology: Did you know that a number is divisible by three if the sum of its digits is divisible by three?
+
+Science & Technology: If you multiply any number with nine and then sum the digits of the product, that sum will always be nine.
+
+Science & Technology: 12345678987654321 is the product of 111111111 x 111111111. Notice the sequence of the numbers 1 to 9 and back to 1.
+
+Science & Technology: Did you know that 2 and 5 are the only primes that end in 2 or 5?
+
+Science & Technology: Want to remember the value of Pi (3.1415926) in easy way? You can do it by counting each word's letters in 'May I have a large container of coffee?'
+
+Shakespeare: Good night, good night! Parting is such sweet sorrow, that I shall say good night till it be morrow.
+
+Shakespeare: Love looks not with the eyes, but with the mind, and therefore is winged Cupid painted blind.
+
+Spiritual: We are not human beings having a spiritual experience. We are spiritual beings having a human experience.
+
+Spiritual: Our scientific power has outrun our spiritual power. We have guided missiles and misguided men.
+
+Spiritual: We buy things we don't need with money we don't have to impress people we don't like.
+
+Star Trek: Space, the final frontier. These are the voyages of the starship Enterprise. Its five-year mission; to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.
+
+Trivia: The world's largest comic book collection belongs to the Library of Congress. They currently hold over 100,000 individual issues.
+
+Trivia: Michael Jackson wanted to play Spider-Man in a movie, so he tried to buy Marvel comics in the 90's.
+
+"""
