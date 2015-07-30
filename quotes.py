cbaca1fd32f99a10ed879df68d976425c176438e
Albert Veli <albert.veli@gmail.com>
2015-07-30 15:28:23 +0200

    Add some more quotes.

--- quotes.py
+++ quotes.py
@@ -16,6 +16,8 @@ Abraham Lincoln: If this is coffee, please bring me some tea; but if this is tea
 
 Abraham Lincoln: Better to remain silent and be thought a fool than to speak out and remove all doubt.
 
+Abraham Lincoln: The best way to destroy an enemy is to make him a friend.
+
 Aristotle: It is the mark of an educated mind to be able to entertain a thought without accepting it.
 
 Aristotle: The worst form of inequality is to try to make unequal things equal.
@@ -52,6 +54,14 @@ Confucius: If you think in terms of a year, plant a seed; if in terms of ten yea
 
 Cybersecurity: A password is like a toothbrush. Choose a good one. Don't share it with anyone. Change it occasionally.
 
+Douglas Adams: He was a dreamer, a thinker, a speculative philosopher... or, as his wife would have it, an idiot.
+
+Douglas Adams: A common mistake when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.
+
+Douglas Adams: In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.
+
+Douglas Adams: For a moment, nothing happened. Then, after a second or so, nothing continued to happen.
+
 Dr. Seuss: Today you are you! That is truer than true! There is no one alive who is you-er than you!
 
 Dr. Seuss: From there to here, and here to there, funny things are everywhere.
@@ -66,9 +76,53 @@ Gandhi: Happiness is when what you think, what you say, and what you do are in h
 
 Gandhi: There is a higher court than courts of justice and that is the court of conscience. It supercedes all other courts.
 
+Goethe: None are more hopelessly enslaved than those who falsely believe they are free.
+
+Goethe: Thinking is easy, acting is difficult, and to put one's thoughts into action is the most difficult thing in the world.
+
+Goethe: There are only two lasting bequests we can hope to give our children. One of these is roots, the other, wings.
+
+Goethe: Do not give in too much to feelings. A overly sensitive heart is an unhappy possession on this shaky earth.
+
+Goethe: The man with insight enough to admit his limitations comes nearest to perfection.
+
+Goethe: Magic is believing in yourself, if you can do that, you can make anything happen.
+
+Goethe: Whoever wishes to keep a secret must hide the fact that he possesses one.
+
+Goethe: All theory, dear friend, is gray, but the golden tree of life springs ever green.
+
+Goethe: If God had wanted me otherwise, He would have created me otherwise.
+
+Goethe: Let everyone sweep in front of his own door, and the whole world will be clean.
+
+Goethe: It is not doing the thing we like to do, but liking the thing we have to do, that makes life blessed.
+
+Goethe: Divide and rule, the politician cries; unite and lead, is watchword of the wise.
+
+Goethe: Thinking is more interesting than knowing, but less interesting than looking.
+
+Goethe: Hatred is something peculiar. You will always find it strongest and most violent where there is the lowest degree of culture.
+
+Goethe: Destiny grants us our wishes, but in its own way, in order to give us something beyond our wishes.
+
+Goethe: All intelligent thoughts have already been thought; what is necessary is only to try to think them again.
+
+Goethe: Which government is the best? The one that teaches us to govern ourselves.
+
+Goethe: Science arose from poetry... when times change the two can meet again on a higher level as friends.
+
+Goethe: What is uttered from the heart alone, will win the hearts of others to your own.
+
 Funny Quotes: I believe that if life gives you lemons, you should make lemonade... And try to find somebody whose life has given them vodka, and have a party.
 
 Funny Quotes: The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.
+
+Funny Quotes: The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails.
+
+Funny Quotes: A common experience is that a problem difficult at night is resolved in the morning after the committee of sleep has worked on it.
+
+Funny Quotes: Always remember that you are absolutely unique. Just like everyone else.
 
 Funny Quotes: Roses are red, violets are blue, I'm schizophrenic, and so am I.
 
@@ -77,6 +131,24 @@ Funny Quotes: If women ran the world we wouldn't have wars, just intense negotia
 Funny Quotes: People are software running on hardware, both built for lives we no longer live.
 
 Funny Quotes: That intelligent creatures exist in outer space is proven by the fact that they have'nt contacted us.
+
+Mark Twain: The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.
+
+Mark Twain: Suppose you were an idiot, and suppose you were a member of Congress; but I repeat myself.
+
+Mark Twain: Truth is stranger than fiction, but it is because fiction is obliged to stick to possibilities; Truth isn't.
+
+Mark Twain: Don't part with your illusions. When they are gone, you may still exist, but you have ceased to live.
+
+Mark Twain: It is curious that physical courage should be so common in the world and moral courage so rare.
+
+Mark Twain: The finest clothing made is a person's own skin, but, of course, society demands something more than this.
+
+Mark Twain: The man who is a pessimist before 48 knows too much; if he is an optimist after it, he knows too little.
+
+Mark Twain: Work consists of whatever a body is obliged to do. Play consists of whatever a body is not obliged to do.
+
+Mark Twain: It is better to deserve honors and not have them than to have them and not deserve them.
 
 Martin Luther King: Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.
 
@@ -88,6 +160,8 @@ Martin Luther King: Law and order exist for the purpose of establishing justice 
 
 Martin Luther King: Nonviolence means avoiding not only external physical violence but also internal violence of spirit. You not only refuse to shoot a man, but you refuse to hate him.
 
+Martin Luther King: That old law about 'an eye for an eye' leaves everybody blind. The time is always right to do the right thing.
+
 Monty Python: We are no longer the knights who say ni! We are now the knights who say ekki-ekki-ekki-pitang-zoom-boing!
 
 Monty Python: Yes. Shrubberies are my trade. I am a shrubber. My name is Rogerthe Shrubber. I arrange, design, and sell shrubberies.
@@ -95,6 +169,38 @@ Monty Python: Yes. Shrubberies are my trade. I am a shrubber. My name is Rogerth
 Monty Python: If she weighs the same as a duck... she's made of wood... (and therefore) a witch!
 
 Monty Python: I cut down trees, I skip and jump, I like to press wild flowers. I put on women's clothing and hang around in bars.
+
+Nietzsche: All things are subject to interpretation. Whichever interpretation prevails at a given time is a function of power and not truth.
+
+Nietzsche: There is always some madness in love. But there is also always some reason in madness.
+
+Nietzsche: Whoever fights monsters should see to it that in the process he does not become a monster. And if you gaze long enough into an abyss, the abyss will gaze back into you.
+
+Nietzsche: The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.
+
+Nietzsche: Perhaps I know best why it is man alone who laughs; he alone suffers so deeply that he had to invent laughter.
+
+Nietzsche: A good writer possesses not only his own spirit but also the spirit of his friends.
+
+Oscar Wilde: There are only two tragedies in life; one is not getting what one wants, and the other is getting it.
+
+Oscar Wilde: Man is least himself when he talks in his own person. Give him a mask, and he will tell you the truth.
+
+Oscar Wilde: Laughter is not at all a bad beginning for a friendship, and it is by far the best ending for one.
+
+Oscar Wilde: Men always want to be a woman's first love - women like to be a man's last romance.
+
+Oscar Wilde: The only way to get rid of temptation is to yield to it... I can resist everything but temptation.
+
+Oscar Wilde: The critic has to educate the public; the artist has to educate the critic.
+
+Oscar Wilde: There is only one thing in life worse than being talked about, and that is not being talked about.
+
+Oscar Wilde: America is the only country that went from barbarism to decadence without civilization in between.
+
+Oscar Wilde: A pessimist is one who, when he has the choice of two evils, chooses both.
+
+Oscar Wilde: I always pass on good advice. It is the only thing to do with it. It is never of any use to oneself.
 
 Science & Technology: Different names for the number 0 include zero, nought, naught, nil, zilch and zip.
 
@@ -112,7 +218,21 @@ Science & Technology: 12345678987654321 is the product of 111111111 x 111111111.
 
 Science & Technology: Did you know that 2 and 5 are the only primes that end in 2 or 5?
 
-Science & Technology: Want to remember the value of Pi (3.1415926) in easy way? You can do it by counting each word's letters in 'May I have a large container of coffee?'
+Science & Technology: The Fibonacci sequence are numbers, each the sum of the previous two. The ratio between numbers approach the golden ratio as they increase.
+
+Science & Technology: A palindrome number reads the same backwards and forward, like 101 which is also prime, and 121 which is also square.
+
+Science & Technology: The number of letters in 'How I wish I could calculate pi' is a way of remembering the first seven digits of pi; 3.141592.
+
+Science & Technology: The number of letters in 'May I have a large container of coffee?' is a way of remembering the first 8 digits of pi; 3.1415926.
+
+Science & Technology: Alice in Wonderland learns the birthday paradox; in a class of 23 pupils the probability that two have the same birthday is more than a half.
+
+Science & Technology: Any number to the power 0 is 1, and 0 to any power is 0. The only unanswered question here is what is 0 to the power 0?
+
+Science & Technology: Navajo soldiers once developed a radio code based on their native language. It was the only uncracked code during the second world war.
+
+Science & Technology: Astronomers know Mars as a backwards planet. Once a year it appears to move backwards in its orbit when the faster orbit of Earth races past Mars.
 
 Shakespeare: Good night, good night! Parting is such sweet sorrow, that I shall say good night till it be morrow.
 
@@ -124,10 +244,72 @@ Spiritual: Our scientific power has outrun our spiritual power. We have guided m
 
 Spiritual: We buy things we don't need with money we don't have to impress people we don't like.
 
+Spiritual: What people say about you is not a reflection of you but a reflection of who they are as human beings.
+
+Spiritual: Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.
+
+Spiritual: Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.
+
+Spiritual: The best teachers are those who show you where to look, but don't tell you what to see.
+
+Spiritual: You never know how strong you are until being strong is the only choice you have left.
+
+Spiritual: Do not go where the path may lead, go instead where there is no path and leave a trail.
+
+Spiritual: Experience is not what happens to you; it's what you do with what happens to you.
+
+Spiritual: True wisdom comes to each of us when we realize how little we understand about life, ourselves, and the world around us.
+
+Spiritual: The young man knows the rules, but the old man knows the exceptions.
+
+Spiritual: Always be a first-rate version of yourself, instead of a second-rate version of somebody else.
+
+Spiritual: It requires wisdom to understand wisdom; the music is nothing if the audience is deaf.
+
+Spiritual: Keep me away from the wisdom which does not cry, the philosophy which does not laugh and the greatness which does not bow before children.
+
+Spiritual: If you set out to be liked, you would be prepared to compromise on anything at any time, and you would achieve nothing.
+
 Star Trek: Space, the final frontier. These are the voyages of the starship Enterprise. Its five-year mission; to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.
+
+Trivia: Lake Baikal is the deepest lake in the world, and holds as much water as all the five Great Lakes of the U.S. combined.
+
+Trivia: Lake Superior, one of the five Great Lakes between the U.S. and the border of Canada, is the largest freshwater lake in the world by surface area, and it's so big it has waves.
+
+Trivia: Scientist believe that diamond rains occur on Neptune and Uranus. The heart of these planets may be a layer of diamonds hundreds of miles thick.
+
+Trivia: Sputnik became the first artificial Earth satellite when it was launched into orbit on 4 October 1957.
+
+Trivia: The first animal sent up into outer space and the first animal to orbit the Earth was the Soviet space dog Laika.
+
+Trivia: Almost 2/3 of earth's surface is covered by water. If the earth was flat, water would cover everything in a layer two miles deep.
 
 Trivia: The world's largest comic book collection belongs to the Library of Congress. They currently hold over 100,000 individual issues.
 
 Trivia: Michael Jackson wanted to play Spider-Man in a movie, so he tried to buy Marvel comics in the 90's.
+
+Trivia: Germany once attempted to confirm that the earth was hollow by sending expeditions in search for supernatural creatures supposedly living underground.
+
+Trivia: Monks in the 16th century recorded seeing a giant explosion on the Moon, likely caused by a large meteor. It was a good thing the Moon was between us and the meteor.
+
+Trivia: About 20 percent of the earths land is made up of desert, and the world's largest desert is the Sahara of North Africa.
+
+Trivia: Olympus Mons is a mountain on Mars, which is about fifteen miles high, three times higher than Mount Everest on earth, and at the top it is 45 miles across.
+
+Trivia: When you think you're standing still remember this fact. Our local group of galaxies is moving at about 1,000,000 miles per hour toward the Virgo Cluster.
+
+Trivia: According to legend Drake's drum can be heard to beat by itself at times when England is at war or significant national events take place.
+
+Trivia: The language of a society changes slowly but steadily with the result that an educated person will not be able to read or understand words in his language written 500 years ago.
+
+Trivia: The Great Pyramid of Giza in Egypt was constructed with enough stone to make a brick wall that could go around the world 20 inches high.
+
+Trivia: When high speed trains pass each other they must slow down, otherwise the low-pressure between them would pull the windows out of their frames.
+
+Trivia: The Hindus of India once believed that the Earth was a huge bowl (to keep the oceans from falling off) held up by giant elephants standing on long pillars.
+
+Trivia: Sound travels 14 times faster through steel than through air. If you need to signal someone quickly standing on a metal bridge, bang on a steel pipe.
+
+Trivia: Steamboat Geyser, located in Yellowstone National Park, is the most powerful geyser in the world and can shoot hot water 300 feet in the air.
 
 """
